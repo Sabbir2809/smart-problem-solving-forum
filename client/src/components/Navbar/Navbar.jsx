6478935748e07891adaaf651d007507c952ba906
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-02-25 08:57:47 +0600

    Bug Fixed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Avatar from '../Avatar/Avatar';
 import logo from './../../assets/logo.png';
-// import search from './../../assets/search.svg';
+import search from './../../assets/search.svg';
 import './Navbar.css';
 
 const Navbar = () => {
@@ -10,16 +10,15 @@ const Navbar = () => {
 
   return (
     <nav className='main-nav'>
-      <h6>Smart Forum</h6>
       <div className='navbar'>
         <Link to='/' className='nav-item nav-logo'>
           <img src={logo} alt='Logo' />
         </Link>
-        {/* <Link to='/about' className='nav-item nav-btn'>
+        <Link to='/about' className='nav-item nav-btn'>
           About
         </Link>
         <Link to='/products' className='nav-item nav-btn'>
-          Products
+          Department
         </Link>
         <Link to='/teams' className='nav-item nav-btn'>
           For Teams
@@ -27,22 +26,22 @@ const Navbar = () => {
         <form>
           <input type='text' placeholder='Search...' />
           <img src={search} className='search-icon' width='18' alt='search' />
-        </form> */}
+        </form>
+        {User === null ? (
+          <Link to='/Auth' className='nav-item nav-links'>
+            Login
+          </Link>
+        ) : (
+          <>
+            <Avatar backgroundColor='#009dff' px='10px' py='7px' borderRadius='50%' color='white'>
+              <Link to='/User' style={{ color: 'white', textDecoration: 'none' }}>
+                S
+              </Link>
+            </Avatar>
+            <button className='nav-item nav-links'>Log Out</button>
+          </>
+        )}
       </div>
-      {User === null ? (
-        <Link to='/Auth' className='nav-item nav-links'>
-          Login
-        </Link>
-      ) : (
-        <>
-          <Avatar backgroundColor='#009dff' px='10px' py='7px' borderRadius='50%' color='white'>
-            <Link to='/User' style={{ color: 'white', textDecoration: 'none' }}>
-              S
-            </Link>
-          </Avatar>
-          <button className='nav-item nav-links'>Log Out</button>
-        </>
-      )}
     </nav>
   );
 };
