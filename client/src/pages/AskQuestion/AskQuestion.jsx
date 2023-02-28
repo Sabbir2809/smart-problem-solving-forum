ff22e7c32cf7d69f5df30943ffcad844109c3d68
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-02-28 22:18:03 +0600

    Ask Question Client Side to Data store MongoDB

--- client/src/pages/AskQuestion/AskQuestion.jsx
+++ client/src/pages/AskQuestion/AskQuestion.jsx
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AskQuestion.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import askQuestion from '../../actions/question';
 
 const AskQuestion = () => {
+  const [questionTitle, setQuestionTitle] = useState();
+  const [questionBody, setQuestionBody] = useState();
+  const [questionTags, setQuestionTags] = useState();
+
+  const dispatch = useDispatch();
+  const User = useSelector((state) => state.currentUserReducer);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log({ questionTitle, questionBody, questionTags });
+    dispatch(
+      askQuestion({ questionTitle, questionBody, questionTags, userPosted: User.result.name }, navigate)
+    );
+  };
+
+  const handleEnter = (e) => {
+    if (e.key === 'Enter') {
+      setQuestionBody(questionBody + '\n');
+    }
+  };
+
   return (
     <div className='ask-question'>
       <div className='ask-question-container'>
         <h1>Ask A Forum Question</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='ask-form-container'>
             {/* Problem Title */}
             <label htmlFor='ask-question-title'>
               <h4>Title</h4>
               <p>Be Specific & Imagine you're asking a Problem to another Person</p>
-              <input type='text' placeholder='problem title' id='ask-question-title' />
+              <input
+                onChange={(e) => {
+                  setQuestionTitle(e.target.value);
+                }}
+                type='text'
+                placeholder='problem title'
+                id='ask-question-title'
+              />
             </label>
             {/* Problem Body */}
             <label htmlFor='ask-question-body'>
               <h4>Body</h4>
               <p>Be Specific & Imagine you're asking a Problem to another Person</p>
-              <textarea type='text' placeholder='problem body' id='ask-question-body' cols='30' rows='10' />
+              <textarea
+                onChange={(e) => {
+                  setQuestionBody(e.target.value);
+                }}
+                onKeyPress={handleEnter}
+                type='text'
+                placeholder='problem body'
+                id='ask-question-body'
+                cols='30'
+                rows='10'
+              />
             </label>
             {/* Problem Tags */}
             <label htmlFor='ask-question-tags'>
               <h4>Tags</h4>
               <p>Add up to 5 Tags to Describe what your Problem is About</p>
-              <input type='text' placeholder='e.g cse swe eee' id='ask-question-tags' />
+              <input
+                onChange={(e) => {
+                  setQuestionTags(e.target.value.split(' '));
+                }}
+                type='text'
+                placeholder='e.g cse swe eee'
+                id='ask-question-tags'
+              />
             </label>
           </div>
           <input type='submit' value='Review Your Question' className='review-btn' />
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth';
 import currentUserReducer from './currentUser';
+import questionsReducer from './question';
 
-export default combineReducers({ authReducer, currentUserReducer });
+export default combineReducers({ authReducer, currentUserReducer, questionsReducer });
